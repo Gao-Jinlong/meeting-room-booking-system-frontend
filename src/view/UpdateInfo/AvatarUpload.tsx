063e2bfa4d453448ee8594dc523e3fac458d5995
Ginlon <2675130594@qq.com>
2024-04-02 23:41:06 +0800

    feat: avatar upload

--- src/view/UpdateInfo/AvatarUpload.tsx
+++ src/view/UpdateInfo/AvatarUpload.tsx
@@ -1,4 +1,33 @@
-import { Button } from "antd";
+import { InboxOutlined } from "@ant-design/icons";
+import { Button, message } from "antd";
+import { DraggerProps } from "antd/es/upload";
+import Dragger from "antd/es/upload/Dragger";
+
+let onChange: Function | undefined = undefined;
+
+const props: DraggerProps = {
+  name: "file",
+  action: "http://localhost:3000/user/upload",
+  onChange(info) {
+    const { status } = info.file;
+    if (status === "done") {
+      console.log(info.file.response);
+      message.success(`${info.file.name} 文件上传成功`);
+      onChange?.(info.file.response.data);
+    } else if (status === "error") {
+      message.error(`${info.file.name} 文件上传失败`);
+    }
+  },
+};
+
+const dragger = (
+  <Dragger {...props}>
+    <p className="ant-upload-drag-icon">
+      <InboxOutlined />
+    </p>
+    <p className="ant-upload-text">点击或拖拽文件到此区域上传文件</p>
+  </Dragger>
+);
 
 export interface AvatarUploadProps {
   value?: string;
@@ -6,6 +35,8 @@ export interface AvatarUploadProps {
 }
 
 export default function AvatarUpload(props: AvatarUploadProps) {
+  onChange = props.onChange;
+
   return props?.value ? (
     <div>
       <img
@@ -14,11 +45,9 @@ export default function AvatarUpload(props: AvatarUploadProps) {
         width="100"
         height="100"
       />
-      <Button>上传</Button>
+      {dragger}
     </div>
   ) : (
-    <div>
-      <Button>上传</Button>
-    </div>
+    <div>{dragger}</div>
   );
 }
